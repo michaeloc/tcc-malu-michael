a4da1a9d92f4a5c12a7b1ea0bcbd2f47d1c9b78d
Maria Luisa Souza Matos <malumatos@gmail.com>
2013-05-31 12:20:57 -0300

    Alteração no nome do package e em alguns arquivos do package com.tccmalumichael

---
+++ Prototipo/src/com/tccmalumichael/ListRide.java
@@ -0,0 +1,127 @@
+/*
+ * Criado por Michael e Malu
+ * Exibe a lista de caronas
+ * 
+ */
+
+package com.tccmalumichael;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tccmalumichael.R;
+import com.tccmalumichael.network.NetworkOperations;
+
+import android.app.ListFragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListRide extends ListFragment
+{
+	private LayoutInflater inflater;
+	public ArrayList<String>rideArrayList;
+	private ArrayAdapter<String> rideArrayAdapter;
+	public static final String SAIDA = "saida";
+	public static final String DESTINO = "destino";
+	static ArrayList<String> listRotasSaida;
+	private ArrayList<String> listRotasDestino;
+	//TextView saida,destino;	
+	public static ListRide newInstance(ArrayList<String> rotaSaida,ArrayList<String>rotaDestino)
+	{
+		 ListRide listRide = new ListRide();
+		 Bundle argumentsBundle = new Bundle();
+		 argumentsBundle.putStringArrayList(SAIDA,rotaSaida);
+		 argumentsBundle.putStringArrayList(DESTINO,rotaDestino);
+		 listRide.setArguments(argumentsBundle);
+		 return listRide;
+		
+	}
+	@Override
+	public void onCreate(Bundle argumentsBundle)
+	{
+		super.onCreate(argumentsBundle);
+		listRotasSaida = getArguments().getStringArrayList(SAIDA);
+		listRotasDestino = getArguments().getStringArrayList(DESTINO);
+	}
+	@Override 
+	public void onSaveInstanceState(Bundle savedInstanceStateBundle)
+	{
+		super.onSaveInstanceState(savedInstanceStateBundle);
+		
+	}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceStateBundle)
+	{
+		super.onActivityCreated(savedInstanceStateBundle);
+		
+		if(listRotasSaida!= null)
+		{
+			setListAdapter(new RideArrayAdapter<String>(getActivity(),R.layout.list,
+					listRotasSaida));
+			
+		}else{
+			rideArrayList = new ArrayList<String>();
+			setListAdapter(new RideArrayAdapter<String>(getActivity(),R.layout.list,
+					rideArrayList));
+		}
+		ListView thisListView = getListView();
+		rideArrayAdapter = (ArrayAdapter<String>)getListAdapter();
+		thisListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		thisListView.setBackgroundColor(Color.WHITE);
+			
+	}
+	//Deprecated...
+	/*private void addDefaultRide(ArrayList <String>rotasSaida,ArrayList <String>rotasDestino)
+	{
+	
+		for(int i = 0; i < rotasSaida.size(); i++)
+		{
+			addRide(rotasSaida.get(i),rotasDestino.get(i));
+			
+		}
+	}
+	public void addRide(String saida, String destino)
+	{
+		rideArrayAdapter.add(saida+"n/"+destino);
+				
+	}*/
+	private class RideArrayAdapter<T> extends ArrayAdapter<String>
+	{
+		
+		private Context context;
+		
+		public RideArrayAdapter(Context context, int textViewResourceId, ArrayList<String> objects)
+		{
+			super(context, textViewResourceId, objects);
+			this.context = context;
+			
+		}
+		
+		@Override
+		public View getView(int position, View convertView,ViewGroup parent){
+			inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View view = inflater.inflate(R.layout.list, parent,false);
+			if(listRotasDestino != null)
+			{
+				 TextView saida = (TextView)view.findViewById(R.id.saidaTextView);
+				 saida.setText(listRotasSaida.get(position));
+				 TextView destino = (TextView)view.findViewById(R.id.destinoTextView);
+				 destino.setText(listRotasDestino.get(position));
+			}
+			return view;
+			
+		}
+	
+	}
+}
---
+++ Prototipo/src/com/tccmalumichael/MainActivity.java
@@ -0,0 +1,261 @@
+package com.tccmalumichael;
+
+import java.util.ArrayList;
+
+import com.tccmalumichael.R;
+import com.tccmalumichael.AddRideDialogFragment.DialogFinishedListener;
+import com.tccmalumichael.LoginActivity.Conectar;
+import com.tccmalumichael.network.NetworkOperations;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.ListFragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SlidingDrawer;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements DialogFinishedListener {
+	private int currentTab ;
+	private ListRide listRideFragments;
+	private static final int CURRENT_CONDITIONS_TAB = 1;
+	private static final String CURRENT_TAB_KEY = "current_tab";
+	private String download;
+	private ArrayList<String> profile;
+	private ArrayList<String> rotaSaida;
+	private ArrayList<String> rotaDestino;
+	private NetworkOperations netOp = NetworkOperations.getNetworkOperations(this);
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ride);
+		//listRideFragments = (ListRide)getFragmentManager().findFragmentById(R.id.replace);
+		setupTabs();
+		
+	}
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.ride, menu);
+		
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if(item.getItemId() == R.id.add_ride_item){
+			showAddRideDialog();
+			return true;
+		}
+		return false;
+	} 
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		download();
+		listRideFragments = ListRide.newInstance(rotaSaida,rotaDestino);
+		changeFragment(listRideFragments);
+	}
+	private void changeFragment(Fragment listRideFragments2)
+	{
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+		ft.replace(R.id.replace, listRideFragments2);
+		ft.commit();
+	}
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceStateBundle){
+		savedInstanceStateBundle.putInt("CURRENT_TAB_KEY", currentTab);
+		super.onSaveInstanceState(savedInstanceStateBundle);
+	}
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceStadeBundle){
+		super.onRestoreInstanceState(savedInstanceStadeBundle);
+		currentTab = savedInstanceStadeBundle.getInt(CURRENT_TAB_KEY);
+	}
+	
+	private void showAddRideDialog()
+	{
+		AddRideDialogFragment newAddRideDialogFragment = new AddRideDialogFragment();
+		FragmentManager thisFragmentManager = getFragmentManager();
+		FragmentTransaction addRideFragmentTransaction = thisFragmentManager.beginTransaction();
+		newAddRideDialogFragment.show(addRideFragmentTransaction, "");
+	}
+	
+	private void setupTabs()
+	{
+		ActionBar rideBar = getActionBar();
+		
+		rideBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		Tab profile = rideBar.newTab();
+		
+		profile.setText(getResources().getString(R.string.profile));
+		profile.setTabListener(rideTabListener);
+		rideBar.addTab(profile);
+		
+		Tab allRide = rideBar.newTab();
+		
+		allRide.setText(getResources().getString(R.string.allRide));
+		allRide.setTabListener(rideTabListener);
+		rideBar.addTab(allRide,true);
+					
+		currentTab = CURRENT_CONDITIONS_TAB;
+				
+	}
+	private void download(){
+		DownloadRide download = new DownloadRide(this);
+		download.execute();
+	}
+	private void selectTab(int position)
+	{
+		currentTab =  position;
+		download();
+		if(currentTab == CURRENT_CONDITIONS_TAB )
+		{
+			listRideFragments = ListRide.newInstance(rotaSaida,rotaDestino);
+			changeFragment(listRideFragments);
+							
+		}else{
+			
+			Profile profile = Profile.newInstance();
+			changeFragment(profile);
+		}
+	}
+	
+	TabListener rideTabListener = new TabListener()
+	{
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			// TODO Auto-generated method stub
+			selectTab(tab.getPosition());
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			// TODO Auto-generated method stub
+		}
+		
+		
+	};
+	@Override
+	public void onDialogFinished(String destino) {
+	
+		
+	}
+	
+	public String [] splitRoute(String rotas)
+	{
+		String []listRide;
+		return listRide = rotas.split("#");
+		
+	}
+	public ArrayList<String>getSaida(String [] rides)
+	{
+		ArrayList<String> saidas = new ArrayList<String>();
+		
+		for(int i = 0; i < rides.length; i++)
+		{
+			if(i % 2 == 0)
+			{
+				saidas.add(rides[i]);
+			}
+		}
+		return saidas;
+	}
+	public ArrayList<String>getDestino(String [] rides)
+	{
+		ArrayList<String> destinos = new ArrayList<String>();
+		for(int i = 0; i < rides.length; i++)
+		{
+			if(i % 2 != 0)
+			{
+				destinos.add(rides[i]);
+			}
+		}
+		return destinos;
+		
+	}
+		
+	public class DownloadRide extends AsyncTask<Void,Void,Boolean>{
+    	
+    	final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+    	
+    	Context mcontext;
+    	
+    	DownloadRide(Context context){
+    		mcontext = context;
+    	}
+    	
+    	@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			dialog.setMessage("Loading...");
+		    dialog.show();
+		}
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			
+			if(currentTab == 0){
+				download = netOp.downloadData("searchRota");
+				rotaSaida = getSaida(splitRoute(download));
+				rotaDestino = getDestino(splitRoute(download));
+				System.out.println(download);
+			}else{
+				
+				download = netOp.downloadData("profile");
+			}
+			if(download == null)
+				return false;
+			
+			return true;
+						
+		}
+		
+		 protected void onPostExecute(Boolean respuesta) {
+			 if (respuesta == true)
+			 {
+				 //getActivity().finish();
+				 
+				 dialog.dismiss();
+				 
+				 //Toast.makeText(getActivity(), "User successfully registered",  Toast.LENGTH_SHORT).show();
+		        						 
+				 //mcontext.startActivity(j);
+				 
+			 }
+			 else{
+				 
+				// Toast.makeText(MainActivity.this, "Registration failure",  Toast.LENGTH_SHORT).show();
+				 
+				 dialog.dismiss();
+			 }
+					 
+		}
+    
+    }
+	
+}
